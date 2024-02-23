77f5d0d072a06fcd64d50b5a2d6f5d2dc9387228
K J <anticream39@yahoo.com>
2024-02-23 18:04:11 -0500

    add new vertex layouts

--- Source/Rendering/VertexLayouts.cs
+++ Source/Rendering/VertexLayouts.cs
@@ -15,3 +15,54 @@ public struct VertexPositionColor
     }
     public const uint SizeInBytes = 24;
 }
+
+public struct VertexPositionColorTexture
+{
+    public const uint SizeInBytes = 36;
+
+    public float PosX;
+    public float PosY;
+    public float PosZ;
+
+    public float ColR;
+    public float ColG;
+    public float ColB;
+    public float ColA;
+
+    public float TexU;
+    public float TexV;
+
+    public VertexPositionTexture(Vector3 pos, RgbaFloat color, Vector2 uv)
+    {
+        PosX = pos.X;
+        PosY = pos.Y;
+        PosZ = pos.Z;
+        ColR = color.R;
+        ColG = color.G;
+        ColB = color.B;
+        ColA = color.A;
+        TexU = uv.X;
+        TexV = uv.Y;
+    }
+}
+
+public struct VertexPositionTexture
+{
+    public const uint SizeInBytes = 20;
+
+    public float PosX;
+    public float PosY;
+    public float PosZ;
+
+    public float TexU;
+    public float TexV;
+
+    public VertexPositionTexture(Vector3 pos, Vector2 uv)
+    {
+        PosX = pos.X;
+        PosY = pos.Y;
+        PosZ = pos.Z;
+        TexU = uv.X;
+        TexV = uv.Y;
+    }
+}
