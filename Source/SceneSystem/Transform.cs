d20ac0ff82dbf07e11c112ce2f26a8a1514a4692
K J <anticream39@yahoo.com>
2024-03-20 15:50:50 -0400

    feat: finish implementing baseline transform

--- Source/SceneSystem/Transform.cs
+++ Source/SceneSystem/Transform.cs
@@ -1,3 +1,5 @@
+using System.Numerics;
+
 namespace WinterEngine.SceneSystem;
 
 public class Transform
@@ -5,11 +7,46 @@ public class Transform
     public Transform Parent => m_Parent;
     private Transform m_Parent;
     
-    // duplicate these for local
-    public Vector3 Position;
-    public Vector3 EulerRotation;
-    public Quaternion Rotation;
-    public Vector3 Scale;
+    public Vector3 Position
+    { 
+        get
+        {
+            if (m_Parent != null)
+                return (LocalPosition + m_Parent.Position);
+            else
+                return (LocalPosition);
+        }
+    }
+    public Vector3 EulerRotation
+    { 
+        get
+        {
+            if (m_Parent != null)
+                return (LocalEulerRotation + m_Parent.EulerRotation);
+            else
+                return (LocalEulerRotation);
+        }
+    }
+    public Quaternion Rotation
+    { 
+        get
+        {
+            if (m_Parent != null)
+                return (LocalRotation + m_Parent.Rotation);
+            else
+                return (LocalRotation);
+        }
+    }
+    public Vector3 Scale
+    { 
+        get
+        {
+            if (m_Parent != null)
+                return (LocalScale + m_Parent.Scale);
+            else
+                return (LocalScale);
+        }
+    }
     
     public Vector3 LocalPosition;
     public Vector3 LocalEulerRotation;
@@ -18,9 +55,10 @@ public class Transform
     
     public void SetParent(Transform parent)
     {
-        
+        m_Parent = parent;
     }
     
+    public Transform() {}
     public Transform(Transform parent)
     {
         m_Parent = parent;
