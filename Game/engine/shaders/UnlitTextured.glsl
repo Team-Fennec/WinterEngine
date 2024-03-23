2eaf545ee7a8a8f0b1e92a2fbb7005eb5adb21af
K J <anticream39@yahoo.com>
2024-03-23 18:49:09 -0400

    feat: Shader additions
    macros and stuff to reduce code duplication

--- Game/engine/shaders/UnlitTextured.glsl
+++ Game/engine/shaders/UnlitTextured.glsl
@@ -3,34 +3,24 @@
 #cull_mode Back
 #depth_clip true
 
+#include "webase.glsl"
+
 VERTEX:
 #include "Buffers.glsl"
 
-layout(location = 0) in vec3 Position;
-layout(location = 1) in vec3 Normal;
-layout(location = 3) in vec4 Color;
-layout(location = 4) in vec2 TexCoords;
-
-layout(location = 0) out vec4 fsin_Color;
-layout(location = 1) out vec2 fsin_texCoords;
-
 void main()
 {
-    vec4 worldPosition = World * vec4(Position, 1);
+    vec4 worldPosition = World * vec4(v_Pos, 1);
     vec4 viewPosition = View * worldPosition;
     vec4 clipPosition = Projection * viewPosition;
     gl_Position = clipPosition;
-    fsin_texCoords = TexCoords;
-    fsin_Color = Color;
+    fsin_texCoords = v_Uv;
+    fsin_Color = v_Col;
 }
 
 FRAGMENT:
-layout(location = 0) in vec4 fsin_Color;
-layout(location = 1) in vec2 fsin_texCoords;
-layout(location = 0) out vec4 fsout_color;
-
-layout(set = 1, binding = 0) uniform texture2D SurfaceTexture;
-layout(set = 1, binding = 1) uniform sampler SurfaceSampler;
+SHADER_PARAM(0, SurfaceTexture, PARAM_TEXTURE2D);
+SHADER_PARAM(1, SurfaceSampler, PARAM_SAMPLER);
 
 void main()
 {
---
+++ Game/engine/shaders/include/webase.glsl
@@ -0,0 +1,38 @@
+/*
+	WinterEngine Base Shader Code
+	
+	Imported by every shader
+
+	(C) 2024 Team Fennec
+*/
+
+// this is automatically defined in the vertex shader
+#if VERTEX_SHADER
+	// if you change this, change the vertex layout as well
+	layout(location = 0) in vec3 Position;
+	layout(location = 1) in vec3 Normal;
+	layout(location = 3) in vec4 Color;
+	layout(location = 4) in vec2 TexCoords;
+	#define v_Pos Position
+	#define v_Norm Normal
+	#define v_Col Color
+	#define v_Uv TexCoords
+
+	layout(location = 0) out vec4 fsin_Color;
+	layout(location = 1) out vec2 fsin_texCoords;
+#else
+	layout(location = 0) in vec4 fsin_Color;
+	layout(location = 1) in vec2 fsin_texCoords;
+	layout(location = 0) out vec4 fsout_color;
+#endif
+
+// param types, wrapped just for linguistic convenience
+#define PARAM_TEXTURE2D texture2D
+#define PARAM_SAMPLER sampler
+#define PARAM_INTEGER int
+#define PARAM_FLOAT float
+#define PARAM_VECTOR2 vec2
+#define PARAM_VECTOR3 vec3
+#define PARAM_VECTOR4 vec4
+
+#define SHADER_PARAM(id, name, type) layout(set = 1, binding = id) uniform type name
