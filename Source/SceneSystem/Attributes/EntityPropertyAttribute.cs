a32d7c42a297fae14363cc595e4231575530f4da
K J <anticream39@yahoo.com>
2024-03-24 00:35:13 -0400

    feat: entity attributes in scenesystem

---
+++ Source/SceneSystem/Attributes/EntityPropertyAttribute.cs
@@ -0,0 +1,15 @@
+﻿namespace WinterEngine.SceneSystem.Attributes;
+
+/// <summary>
+/// Binds an actor class to an entity class for the map editor & internal entity system.
+/// </summary>
+[AttributeUsage(AttributeTargets.Field|AttributeTargets.Property, AllowMultiple = false, Inherited = false)]
+public class EntityPropertyAttribute : Attribute
+{
+    public string PropName;
+
+    public EntityPropertyAttribute(string propName)
+    {
+        PropName = propName;
+    }
+}
