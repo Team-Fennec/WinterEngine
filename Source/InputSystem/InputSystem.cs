85750a570f91667c68ab4b2c0e6ba8b767aad5e7
K J <anticream39@yahoo.com>
2024-03-24 11:47:59 -0400

    feat: add stubbed input system class

---
+++ Source/InputSystem/InputSystem.cs
@@ -0,0 +1,36 @@
+namespace WinterEngine.InputSystem;
+
+public static class InputSystem
+{
+	// Keyboard key state
+	public bool KeyCheckPressed()
+	{
+		throw new NotImplementedException();
+	}
+
+	public bool KeyCheckReleased()
+	{
+		throw new NotImplementedException();
+	}
+
+	public bool KeyCheck()
+	{
+		throw new NotImplementedException();
+	}
+
+	// Gamepad button state
+	public bool GamepadCheckPressed()
+	{
+		throw new NotImplementedException();
+	}
+
+	public bool GamepadCheckReleased()
+	{
+		throw new NotImplementedException();
+	}
+
+	public bool GamepadCheck()
+	{
+		throw new NotImplementedException();
+	}
+}
