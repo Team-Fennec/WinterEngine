0f6be69a81f1cc49eff4d0b90be952b330d94d52
K J <anticream39@yahoo.com>
2024-03-24 16:16:15 -0400

    fix(testgame): forgotten inputactions

--- Source/Game/testgame/TestGameModule.cs
+++ Source/Game/testgame/TestGameModule.cs
@@ -1,6 +1,7 @@
 using log4net;
 using TestGame.Entities;
 using WinterEngine.Core;
+using WinterEngine.InputSystem;
 using WinterEngine.SceneSystem;
 
 namespace TestGame;
@@ -12,6 +13,28 @@ public class TestGameModule : GameModule
     public override void Startup()
     {
         Log.Info("Staring up game");
+
+        // register freecam actions
+        InputAction moveUpAction = new InputAction("MoveUp");
+        InputAction moveDownAction = new InputAction("MoveDown");
+        InputAction moveLeftAction = new InputAction("MoveLeft");
+        InputAction moveRightAction = new InputAction("MoveRight");
+        InputAction lookLeftAction = new InputAction("LookLeft");
+        InputAction lookRightAction = new InputAction("LookRight");
+
+        moveUpAction.AddBinding(Veldrid.Key.W);
+        moveDownAction.AddBinding(Veldrid.Key.S);
+        moveLeftAction.AddBinding(Veldrid.Key.A);
+        moveRightAction.AddBinding(Veldrid.Key.D);
+        lookLeftAction.AddBinding(Veldrid.Key.Left);
+        lookRightAction.AddBinding(Veldrid.Key.Right);
+
+        InputManager.RegisterAction(moveUpAction);
+        InputManager.RegisterAction(moveDownAction);
+        InputManager.RegisterAction(moveLeftAction);
+        InputManager.RegisterAction(moveRightAction);
+        InputManager.RegisterAction(lookLeftAction);
+        InputManager.RegisterAction(lookRightAction);
 
         // Create test example scene
         m_TestScene = new Scene("Testing Scene");
