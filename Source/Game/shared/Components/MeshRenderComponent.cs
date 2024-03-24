84e0f1f255e453c261b464e556a5e6194bf78acb
K J <anticream39@yahoo.com>
2024-03-24 16:07:34 -0400

    wip: meshrendercomponent

--- Source/Game/shared/Components/MeshRenderComponent.cs
+++ Source/Game/shared/Components/MeshRenderComponent.cs
@@ -1,1 +1,22 @@
-using System;
+using WinterEngine.MaterialSystem;
+using WinterEngine.RenderSystem;
+using WinterEngine.SceneSystem;
+
+using gMaterialSystem = WinterEngine.MaterialSystem.MaterialSystem;
+
+namespace WinterEngine.Components;
+
+public class MeshRenderComponent : EntityComponent
+{
+    private TexturedMeshRO m_RenderModel;
+
+    public override void Awake()
+    {
+        // load md3 resource, grab material
+    }
+
+    public override void OnEnable()
+    {
+        Renderer.AddRenderObject(m_RenderModel);
+    }
+}
