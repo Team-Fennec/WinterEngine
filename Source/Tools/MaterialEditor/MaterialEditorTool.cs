47c3d830a5f8bd18bc9185ed55524ff852082132
K J <anticream39@yahoo.com>
2024-03-24 22:46:39 -0400

    feat: base material editor tool

---
+++ Source/Tools/MaterialEditor/MaterialEditorTool.cs
@@ -0,0 +1,29 @@
+﻿using WinterEngine.ToolsFramework;
+
+namespace MaterialEditor;
+
+public sealed class MaterialEditorTool : EngineTool
+{
+    public override string ToolName => "Material Editor";
+
+    public override void GameThink(double deltaTime)
+    {
+        throw new NotImplementedException();
+    }
+
+    public override void Init()
+    {
+        throw new NotImplementedException();
+    }
+
+    public override void OnLayout()
+    {
+        
+    }
+
+    public override void Shutdown()
+    {
+        throw new NotImplementedException();
+    }
+}
+
